1ee56da561322fd2b0531a5ba8bd28c9de7ca4fe
Darrell Harmon <darrell@harmoninstruments.com>
2015-01-03 20:40:00 -0700

    cleanup code

--- cordic/test.py
+++ cordic/test.py
@@ -25,60 +25,56 @@ from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge, ReadOnly, Event
 from cocotb.result import TestFailure, ReturnValue
 
-amult = 2**18/(2.0*np.pi)
-dmult = 2**18-1.0
-stages = 18
-gain = 1.6467
+from config import *
+
+amult = 2**stages/(2.0*np.pi)
+dmult = 2**(nbits_din-1)-1.0
+gain = 1.64676025811
+count = 5000
 
 @cocotb.coroutine
-def do_rotate(dut, din):
-#This coroutine performs a write of the RAM
+def do_translate(dut, din):
     yield RisingEdge(dut.clock)
     in_scaled = np.round(din*dmult)
     dut.in_re = int(np.real(in_scaled))
     dut.in_im = int(np.imag(in_scaled))
-    for i in range(stages):
-        yield RisingEdge(dut.clock)
     yield ReadOnly() # Wait until all events have executed for this timestep
     result_mag = int(dut.out_mag.value.integer)*1.0/dmult
     result_angle = int(dut.out_angle.value.signed_integer)*1.0/amult
-    expected = gain * din
-    expected_mag = np.abs(expected)
-    expected_angle = np.angle(expected)
-    error_angle = result_angle - expected_angle
-    error_mag = result_mag - expected_mag
-    #print result_mag, expected_mag, error_mag
-    #print result_angle, expected_angle, error_angle, error_angle * amult
-    #error = np.abs(result - expected) / np.abs(expected)
-    if error_angle > 1e-4:
-        dut.log.error("FAIL: din = {}, angle = {}, result = {}, expected = {}, error = {}".format(din, expected_angle, result_angle, expected_mag, error_mag))
-    #    raise TestFailure("incorrect result")
-    raise ReturnValue((error_mag, error_angle))
+    raise ReturnValue((result_mag, result_angle))
 
 @cocotb.test()
 def run_test(dut):
     """Test CORDIC rotate"""
     clock = dut.clock
     a = cocotb.fork(Clock(clock, 2500).start())
-    count = 1000
     angles = np.random.random(count) * 2.0 * np.pi
     din = np.exp(1j*angles)
-    msum = 0.0
-    asum = 0.0
-    mrms = 0.0
-    arms = 0.0
-    for dinv in din:
-        (m, a) = yield do_rotate(dut, dinv)
-        msum += m
-        asum += a
-        mrms += m*m
-        arms += a*a
-    msum /= count
-    asum /= count
-    mrms /= count
-    arms /= count
-    mrms = np.sqrt(mrms)
-    arms = np.sqrt(arms)
+    expected = gain * din
+    expected_mag = np.abs(expected)
+    expected_angle = np.angle(expected)
+    result_mag = np.zeros(count)
+    result_angle = np.zeros(count)
+    for i in range(count+pipestages):
+        if i < count:
+            (m, a) = yield do_translate(dut, din[i])
+        else:
+            (m, a) = yield do_translate(dut, 0.0)
+        if i >= pipestages:
+            result_angle[i-pipestages] = a
+            result_mag[i-pipestages] = m
+
+    error_angle = result_angle - expected_angle
+    error_mag = result_mag - expected_mag
+    msum = np.sum(error_mag)/count
+    asum = np.sum(error_angle)/count
+    mrms = np.sqrt(np.sum(np.square(error_mag))/count)
+    arms = np.sqrt(np.sum(np.square(error_angle))/count)
+
+    print "maximum magnitude error =", np.max(error_mag)
+    print "maximum magnitude error (LSB) =", np.max(error_mag) * dmult
+    print "maximum angle error (radian) =", np.max(error_angle)
+    print "maximum angle error (LSB) =", np.max(error_angle) * amult
     print "mean magnitude error =", msum
     print "mean magnitude error (LSB) =", msum * dmult
     print "mean angle error (radian) =", asum
@@ -87,3 +83,6 @@ def run_test(dut):
     print "RMS magnitude error (LSB) =", mrms * dmult
     print "RMS angle error (radian) =", arms
     print "RMS angle error (LSB) =", arms * amult
+
+    if np.max(error_angle) > 1e-4:
+        dut.log.error("FAIL")
