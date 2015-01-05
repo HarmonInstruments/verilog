873a4e02aa5e73f9d8aa46a8e9050e1e44d50e9b
Darrell Harmon <darrell@harmoninstruments.com>
2015-01-05 16:18:34 -0700

    improving mapping to DSP48

--- sincos/cos_int.v
+++ sincos/cos_int.v
@@ -21,11 +21,11 @@
 // 7 clocks
 module cosine_int
   (
-   input 		       c,
-   input [NBA-1:0] 	       a,
-   input [2*NBO-12:0] 	       rom_d,
-   output [9:0] 	       rom_a,
-   output reg signed [NBO-1:0] o
+   input 		   c,
+   input [NBA-1:0] 	   a,
+   input [NBO+NBM-2:0] 	   rom_d,
+   output [9:0] 	   rom_a,
+   output signed [NBO-1:0] o
    );
 
    parameter NBA = 22; // bits in angle in - all but 12 are interpolated
@@ -33,46 +33,61 @@ module cosine_int
    localparam NBP = NBA-12; // bits to interpolator
    localparam NBM = NBO-10; // bits in interpolation multiply value
 
-   reg [5:0]     sign; // these two pipe stages are the ROM lookup
-   reg [NBP-1:0] alow_0;
-   reg [NBP-1:0] alow_1;
-   reg [NBP-1:0] alow_2; // these next 4 pipe stages are the DSP48
-   reg [NBO-2:0] coarse_2;
-   reg [NBM-1:0]  fine_2;
-   reg [NBP-1:0] alow_3;
-   reg [NBO-2:0] coarse_3;
-   reg [NBM-1:0]  fine_3;
-   reg [NBP+NBM-1:0] p_4;
-   reg [NBO-2:0] coarse_4;
-   reg [NBO+NBP-2:0] p_5;
+   reg [1:0] 		   sign; // these two pipe stages are the ROM lookup
+   reg [NBP-1:0] 	   alow_0;
+   reg signed [NBP:0] 	   alow_1;
+   reg signed [NBO-1:0]    coarse_2;
 
-   wire signed [NBO+NBP-1:0] s_5 = sign[5] ? 2'sd0 - $signed({1'b0,p_5}) : p_5;
-
-   wire [NBA-3:0]     a_adj = a[NBA-2] ? ~ a[NBA-3:0] : a[NBA-3:0];
+   wire [NBA-3:0] 	a_adj = a[NBA-2] ? ~ a[NBA-3:0] : a[NBA-3:0];
 
    assign rom_a = a_adj >> NBP;
 
+   wire [NBO-1:0]     coarse_1 = (rom_d >> NBM);
+
+   dsp_wrap_cos_int
+     #(.NBA(NBP+1), .NBB(NBM+1), .NBD(NBP+NBO), .NBP(NBO), .S(NBP))
+   dsp_i
+     (.c(c),
+      .a(alow_1),
+      .b({1'b0, rom_d[NBM-1:0]}),
+      .d({coarse_2, 1'b1, {(NBP-1){1'b0}}}),
+      .p(o));
+
    always @ (posedge c) begin
-      sign <= {sign[4:0], a[NBA-1] ^ a[NBA-2]};
-
+      sign <= {sign[0], a[NBA-1] ^ a[NBA-2]};
       alow_0 <= a_adj[NBP-1:0];
-
-      alow_1 <= alow_0;
-
-      alow_2 <= alow_1;
-      fine_2 <= rom_d[NBM-1:0];
-      coarse_2 <= rom_d >> NBM;
-
-      alow_3 <= alow_2;
-      fine_3 <= fine_2;
-      coarse_3 <= coarse_2;
-
-      p_4 <= fine_3 * alow_3;
-      coarse_4 <= coarse_3;
-
-      p_5 <= ({coarse_4, 1'b1} << (NBP-1)) - p_4;
-
-      o <= s_5 >>> NBP;
+      alow_1 <= sign[0] ? alow_0 : 2'sd0 - $signed({1'b0, alow_0});
+      coarse_2 <= sign[1] ? 2'sd0 - $signed(coarse_1) : coarse_1;
    end
 
+endmodule
+
+//p = d + a * c
+module dsp_wrap_cos_int
+  (
+   input 		   c,
+   input signed [NBA-1:0]  a,
+   input signed [NBB-1:0]  b,
+   input signed [NBD-1:0]  d,
+   output signed [NBP-1:0] p);
+
+   parameter NBA = 18;
+   parameter NBB = 18;
+   parameter NBD = 48;
+   parameter NBP = 48;
+   parameter S = 0; // Shift
+
+   reg signed [NBA-1:0]     a_1;
+   reg signed [NBB-1:0]     b_1;
+   reg signed [NBD-1:0]     d_2;
+   reg signed [NBA+NBB-1:0] m_2;
+   reg signed [NBP+S-1:0]   p_3;
+   always @ (posedge c) begin
+      a_1 <= a;
+      b_1 <= b;
+      d_2 <= d;
+      m_2 <= a_1 * b_1;
+      p_3 <= d_2 + m_2;
+   end
+   assign p = p_3[NBP+S-1:S];
 endmodule
